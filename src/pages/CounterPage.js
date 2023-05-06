3d23ddbc1d4c7bef16922b6736a18b304bb4c29f
Piterxus <piterxus@gmail.com>
2023-05-06 17:36:38 +0200

    End React

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,6 +1,7 @@
 import { useReducer } from 'react';
 import Button from '../components/Button';
 import Panel from '../components/Panel';
+import { produce } from 'immer';
 // import useCounter from '../hooks/use-counter';  
 
 const INCREMENT_COUNT = 'increment';
@@ -14,35 +15,26 @@ const reducer = (state, action) => {
 
     switch (action.type) {
         case INCREMENT_COUNT:
-            return {
-                ...state,
-                count: state.count + 1
-            }
-        case SET_VALUE_TO_ADD:
-            return {
-                ...state,
-                valueToAdd: action.payload
-            }
+            state.count = state.count + 1;
+            return; case SET_VALUE_TO_ADD:
+            state.valueToAdd = action.payload;
+            return;
         case DECREMENT_COUNT:
-            return {
-                ...state,
-                count: state.count - 1
-            }
+            state.count = state.count - 1;
+            return;
         case ADD_VALUE_TO_COUNT:
-            return {
-                ...state,
-                count: state.count + state.valueToAdd,
-                valueToAdd: 0
-            }
+            state.count = state.count + state.valueToAdd;
+            state.valueToAdd = 0;
+            return;
         default:
-            return state;
+            return;
     }
 }
 
 function CounterPage({ initialCount }) {
     //  const [count, setCount]= useState(initialCount);
     //  const [valueToAdd, setValueToAdd] = useState(0);  
-    const [state, dispatch] = useReducer(reducer,
+    const [state, dispatch] = useReducer(produce(reducer),
         {
             count: initialCount,
             valueToAdd: 0
@@ -74,7 +66,7 @@ function CounterPage({ initialCount }) {
         dispatch({
             type: ADD_VALUE_TO_COUNT
         })
-     }
+    }
 
 
 
